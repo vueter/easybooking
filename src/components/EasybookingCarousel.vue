25368a71ac395d00631477b9b65e94d38202ae22
Beknazarov <tensor2flow@gmail.com>
2019-05-18 15:15:33 +0500

    change carausel

--- src/components/EasybookingCarousel.vue
+++ src/components/EasybookingCarousel.vue
@@ -1,9 +1,9 @@
 <template>
   <v-container>
     <v-carousel>
-      <v-carousel-item src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"></v-carousel-item>
-      <v-carousel-item src="https://cdn.vuetifyjs.com/images/carousel/sky.jpg"></v-carousel-item>
-      <v-carousel-item src="https://cdn.vuetifyjs.com/images/carousel/bird.jpg"></v-carousel-item>
+      <v-carousel-item src="https://raw.githubusercontent.com/vueter/easybooking/master/src/assets/image/slide.png"></v-carousel-item>
+      <v-carousel-item src="https://raw.githubusercontent.com/vueter/easybooking/master/src/assets/image/slide.png"></v-carousel-item>
+      <v-carousel-item src="https://raw.githubusercontent.com/vueter/easybooking/master/src/assets/image/slide.png"></v-carousel-item>
     </v-carousel>
   </v-container>
 </template>
