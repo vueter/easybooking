2dbb995fcbd2bda5686dc5891acc595c246599ed
tensor2flow <tensor2flow@gmail.com>
2019-07-15 01:19:23 +1000

    date picker

--- src/easybooking/components/comboboxes/EasybookingDateRange.vue
+++ src/easybooking/components/comboboxes/EasybookingDateRange.vue
@@ -1,41 +1,21 @@
 <template>
   <v-menu v-bind:close-on-content-click="false" offset-y ref="date-range" max-width="810px" class="easybooking--double-datepicker">
     <template v-slot:activator="{ on }">
-      <v-layout row class="easybooking--double-combobox" v-on:click="open">
-        <v-flex class="easybooking--double-combobox-field">
-          <input type="text" class="first" ref="departure" readonly>
+      <v-layout row class="easybooking--double-combobox">
+        <v-flex class="easybooking--double-combobox-field" v-on:click="open('departure')">
+          <input type="text" class="first" v-model="result.departure" readonly>
         </v-flex>
         <v-btn icon class="easybooking--double-combobox-icon-btn" v-bind:ripple="false">
           <v-icon color="primary">date_range</v-icon>
         </v-btn>
-        <v-flex class="easybooking--double-combobox-field">
-          <input type="text" class="last" ref="arrival" readonly>
+        <v-flex class="easybooking--double-combobox-field" v-on:click="open('arrival')">
+          <input type="text" class="last" v-model="result.arrival" readonly>
         </v-flex>
       </v-layout>
     </template>
     <v-card>
       <v-card-text>
-        <v-layout row>
-          <v-flex v-bind:md6="type" v-bind:md12="!type">
-            <v-layout row>
-              <v-flex>
-                <font class="date-switch-label" v-bind:class="type ? '' : 'active-date-swwitch-label'">В одну сторону</font>
-              </v-flex>
-              <v-switch v-model="type" class="date-switch" success color="primary"></v-switch>
-              <v-flex>
-                <font class="date-switch-label" v-bind:class="type ? 'active-date-swwitch-label' : ''">Туда-обратно</font>
-              </v-flex>
-            </v-layout>
-          </v-flex>
-        </v-layout>
-        <v-layout row>
-          <v-flex md6>
-            <v-date-picker no-title />
-          </v-flex>
-          <v-flex md6>
-            <v-date-picker no-title v-if="type" />
-          </v-flex>
-        </v-layout>
+        <v-date-picker no-title v-model="result[activeTarget]" v-bind:min="min"/>
       </v-card-text>
     </v-card>
   </v-menu>
@@ -55,12 +35,26 @@ export default {
       default: true
     }
   },
+  data: () => ({
+    activeTarget: 'departure',
+    now: new Date().toISOString().substr(0, 10),
+    result: {
+      departure: null,
+      arrival: null
+    }
+  }),
   methods: {
-    open () {
+    open (target) {
       const menu = this.$refs['date-range']
+      this.activeTarget = target
       menu.runDelay('open', () => {
         menu.isActive = true
       })
+    }
+  },
+  computed:{
+    min(){
+      return this.activeTarget !== 'departure' ? ( this.result.departure !== null ? this.result.departure : this.now ) : this.now
     }
   }
 }
