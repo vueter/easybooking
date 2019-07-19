dc323d8fa62e856872f60ea8fbd8a4a3ebc1648a
Umar <mamatov.umar@mail.ru>
2019-07-19 11:02:41 +0300

    commit

---
+++ src/easybooking/mixins/utils.js
@@ -0,0 +1,46 @@
+export default {
+
+    methods: {
+        formatPrice(price) {
+            price = Math.ceil(price);
+            if (price === null) {
+                return "Билетов нет";
+            } else {
+                price = Math.ceil(price);
+                price = new Intl.NumberFormat("ru-RU").format(price);
+                return price;
+            }
+        },
+        formatFilterDate(timestamp) {
+            var a = new Date(timestamp * 1000);
+            var months_arr = [
+                "Янв",
+                "Фев",
+                "Мар",
+                "Апр",
+                "Май",
+                "Июн",
+                "Июл",
+                "Авг",
+                "Сен",
+                "Окт",
+                "Ноя",
+                "Дек"
+            ];
+            var week_arr = ["вс", "пн", "вт", "ср", "чт", "пт", "сб"];
+            var year = a.getFullYear();
+            var month = months_arr[a.getMonth()];
+            var month2 = a.getMonth();
+            var week = week_arr[a.getDay()];
+            var date = a.getDate();
+            var hour = a.getHours();
+            var min = a.getMinutes();
+            var sec = a.getSeconds();
+            var time = hour + ":" + (min < 10 ? 0 : '') + min + ", " + date + " " + month;
+            return time;
+        },
+    },
+
+
+
+}
