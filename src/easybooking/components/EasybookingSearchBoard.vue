f10f84959d8b231227d712134048c9bdf13a799d
Umar <mamatov.umar@mail.ru>
2019-07-20 16:04:18 +0500

    commit

--- src/easybooking/components/EasybookingSearchBoard.vue
+++ src/easybooking/components/EasybookingSearchBoard.vue
@@ -1,35 +1,41 @@
 <template>
   <v-form class="easybooking-search-board">
     <template v-if="isMulti">
-      <v-layout row wrap class="easybooking-search-board-responsive" v-for="i in num_routes" v-bind:key="'routes_' + i">
-        <v-flex md9 sm12>
+      <v-layout
+        row
+        wrap
+        class="easybooking-search-board-responsive e-muti-form"
+        v-for="i in num_routes"
+        v-bind:key="'routes_' + i"
+      >
+        <v-flex md9 sm8>
           <easybooking-double-combobox ref="location" />
         </v-flex>
-        <v-flex md3 sm12>
-          <easybooking-date-range ref="dates" single/>
+        <v-flex md3 sm4>
+          <easybooking-date-range ref="dates" single />
         </v-flex>
       </v-layout>
-      <v-layout row>
+      <v-layout class="easybooking-search-board-responsive" row>
         <v-flex md6 pr-1>
           <easybooking-menu-passanger ref="passengers" />
         </v-flex>
         <v-flex md3 pr-1 pl-2>
           <v-btn
-              float
-              block
-              depressed
-              color="primary"
-              outline
-              class="easybooking--search-btn"
-              v-on:click="num_routes < 4 ? num_routes = num_routes + 1 : num_routes = num_routes"
-            >Продолжить маршрут</v-btn>
+            float
+            block
+            depressed
+            color="primary"
+            outline
+            class="easybooking--search-btn search-form-alt-btn"
+            v-on:click="num_routes < 4 ? num_routes = num_routes + 1 : num_routes = num_routes"
+          >Продолжить маршрут</v-btn>
         </v-flex>
         <v-flex md3 pl-2>
           <slot name="activator"></slot>
         </v-flex>
       </v-layout>
     </template>
-    <v-layout v-else row wrap class="easybooking-search-board-responsive" >
+    <v-layout v-else row wrap class="easybooking-search-board-responsive">
       <slot>
         <v-flex md5 sm12>
           <easybooking-double-combobox ref="location" />
@@ -41,7 +47,7 @@
           <easybooking-menu-passanger ref="passengers" />
         </v-flex>
       </slot>
-      <v-flex md2 xs12>
+      <v-flex md2 xs12 class="search-form-btn">
         <v-btn
           float
           block
@@ -50,22 +56,24 @@
           class="easybooking--search-btn"
           v-on:click="search"
           v-bind:loading="isLoading"
-        >Search</v-btn>
+        >Поиск</v-btn>
       </v-flex>
     </v-layout>
-    <v-layout row>
-      <v-flex md3>
-        <v-checkbox label="Сложный маршрут" v-model="isMulti" color="primary"/>
+    <v-layout row class="search-form-items">
+      <v-flex sm4 md3 class="text-left">
+        <v-btn class="e-ctrip" flat @click="isMulti = !isMulti">
+          <img src="@/assets/image/triangle.png" /> Сложный маршрут
+        </v-btn>
       </v-flex>
-      <v-flex md9>
-        <v-checkbox label="+/- 1 день" v-model="flexible" color="primary"/>
+      <v-flex sm4 md9 class="text-left" v-if="!isMulti">
+        <v-checkbox class="e-checkbox" label="+/- 1 день" v-model="flexible" color="primary" />
       </v-flex>
     </v-layout>
   </v-form>
 </template>
 <script>
 export default {
-  name: 'easybooking-search-board',
+  name: "easybooking-search-board",
   data: () => ({
     flexible: false,
     isMulti: false,
@@ -76,73 +84,150 @@ export default {
   methods: {
     search() {
       this.routes = this.getFlights();
-      if(this.routes){
-        this.isLoading = true
-        this.$store.commit('setSearchParameters', this.routes)
+      if (this.routes) {
+        this.isLoading = true;
+        this.$store.commit("setSearchParameters", this.routes);
         this.$etm.search(this.routes, (error, result) => {
           if (error) {
-            this.$etm.alert('Could not search')
+            this.$etm.alert("Could not search");
           } else {
-            this.$router.push({ path: '/offers/' + result.request_id });
+            this.$router.push({ path: "/offers/" + result.request_id });
           }
         });
       }
     },
-    getFlights () {
-      if(this.isMulti){
-        var directions = []
-        for(const i in this.$refs['location']){
-          var direction = this.$refs['location'][i].getDirection()
-          var dates = this.$refs['dates'][i].getDates()
+    getFlights() {
+      if (this.isMulti) {
+        var directions = [];
+        for (const i in this.$refs["location"]) {
+          var direction = this.$refs["location"][i].getDirection();
+          var dates = this.$refs["dates"][i].getDates();
           directions.push({
             departure_code: direction.departure_code,
             arrival_code: direction.arrival_code,
             date: dates.departure
-          })
+          });
         }
-      }
-      else{
-        var direction = this.$refs['location'].getDirection()
-        var dates = this.$refs['dates'].getDates()
-        var directions = []
-        if(dates.departure){
+      } else {
+        var direction = this.$refs["location"].getDirection();
+        var dates = this.$refs["dates"].getDates();
+        var directions = [];
+        if (dates.departure) {
           directions.push({
             departure_code: direction.departure_code,
             arrival_code: direction.arrival_code,
             date: dates.departure
-          })
+          });
           if (dates.arrival) {
             directions.push({
               departure_code: direction.arrival_code,
               arrival_code: direction.departure_code,
               date: dates.arrival
-            })
+            });
           }
         }
       }
-      var passengers = this.$refs['passengers'].getPassengers()
-      var flights = { directions, direct: false, flexible: this.flexible, max_price: '', airlines: [], fare_types: ['PUB', 'NEG'] }
-      
-      return Object.assign(flights, passengers)
+      var passengers = this.$refs["passengers"].getPassengers();
+      var flights = {
+        directions,
+        direct: false,
+        flexible: this.flexible,
+        max_price: "",
+        airlines: [],
+        fare_types: ["PUB", "NEG"]
+      };
+
+      return Object.assign(flights, passengers);
     }
   }
-}
+};
 </script>
 <style lang="scss">
-.easybooking-search-board{
-  &-responsive{
+.e-ctrip {
+  padding: 0 !important;
+  margin: 0 !important;
+  text-transform: initial;
+  font-size: 13px;
+  line-height: 15px;
+  color: #777777 !important;
+  font-weight: 400;
+  height: auto;
+
+  &:before {
+    display: none;
+  }
+  &:hover,
+  &:focus {
+    background-color: white;
+  }
+  img {
+    margin-right: 5px;
+  }
+  .v-ripple__container {
+    display: none;
+  }
+}
+.e-multi-form{
+  margin-bottom: 15px !important;
+}
+.e-checkbox {
+  margin: 0;
+  padding: 0;
+  label {
+    font-size: 13px;
+    line-height: 15px;
+    color: #777777;
+  }
+  i {
+    font-size: 22px;
+    color: #0fb8d3 !important;
+  }
+  .v-input__slot {
+    margin-bottom: 0 !important;
+  }
+  .v-messages {
+    display: none;
+  }
+}
+.search-form-alt-btn {
+  height: 44px !important;
+  .v-btn__content {
+    text-transform: initial;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 18px;
+  }
+}
+.easybooking-search-board {
+  &-responsive {
     margin-left: -5px;
     margin-right: -5px;
-    &>*{
+    & > * {
       padding: 5px 5px;
       margin: 0;
     }
   }
-  .easybooking--double-combobox{
+  .easybooking--double-combobox {
     margin-top: 0;
   }
-  .v-btn--block{
+  .v-btn--block {
     margin: 0;
+  }
+}
+.search-form-btn {
+  .v-btn__content {
+    text-transform: initial;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 18px;
+  }
+}
+@media screen and (max-width: 959px) {
+  .search-form-btn {
+    margin-top: 47px;
+  }
+  .search-form-items {
+    margin-top: -100px;
   }
 }
 </style>
--- src/views/Offers.vue
+++ src/views/Offers.vue
@@ -1,116 +1,153 @@
 <template>
-    <div class="bg-white">
-        <easybooking-toolbar v-bind:languages="languages" v-bind:actions="actions" v-bind:statics="statics"/>
-        <v-container>
-            <easybooking-search-board ref="search-board" />
-        </v-container>
-        <v-layout class="offers">
-            <v-container>
-                <v-layout row>
-                  <v-flex md12>
-                    <easybooking-stepper-card />
-                  </v-flex>
-                </v-layout>
-                <v-layout row>
-                    <v-flex md4 class="e-sidebar">
-                        <easybooking-subscribe-card />
-                        <easybooking-filter-card v-if="filterOptions" v-bind:filter-options="filterOptions" v-bind:reset="reset"/>
-                    </v-flex>
-                    <v-flex md8>
-                      <easybooking-ticket-ff ref="fareFamily"/>
-                      <easybooking-ticket-rules ref="rules" />
-                      <easybooking-ticket-card
-                        v-for="ticket in tickets" 
-                        v-bind:key="ticket.segment_id" 
-                        v-bind:ticket="ticket" 
-                        v-bind:ff="$refs['fareFamily']"
-                        v-bind:rules="$refs['rules']"/>
-                      <v-btn v-if="$easybooking.match.tickets && filterOptions && $easybooking.match.tickets.length > filterOptions.num_tickets" large color="primary" rounded block v-on:click="filterOptions.num_tickets += 10">Show more</v-btn>
-                    </v-flex>
-                </v-layout>
-            </v-container>
+  <div class="bg-white">
+    <easybooking-toolbar
+      v-bind:languages="languages"
+      v-bind:actions="actions"
+      v-bind:statics="statics"
+    />
+    <v-container>
+      <easybooking-search-board ref="search-board" />
+    </v-container>
+    <v-layout class="offers">
+      <v-container>
+        <v-layout row>
+          <v-flex md12>
+            <easybooking-stepper-card />
+          </v-flex>
         </v-layout>
-        <easybooking-footer>
-            {{languages}}
-        </easybooking-footer>
-    </div>
+        <v-layout row>
+          <v-flex md4 class="e-sidebar">
+            <easybooking-subscribe-card />
+            <easybooking-filter-card
+              v-if="filterOptions"
+              v-bind:filter-options="filterOptions"
+              v-bind:reset="reset"
+            />
+          </v-flex>
+          <v-flex md8>
+            <easybooking-ticket-ff ref="fareFamily" />
+            <easybooking-ticket-rules ref="rules" />
+            <easybooking-ticket-card
+              v-for="ticket in tickets"
+              v-bind:key="ticket.segment_id"
+              v-bind:ticket="ticket"
+              v-bind:ff="$refs['fareFamily']"
+              v-bind:rules="$refs['rules']"
+            />
+            <v-btn
+              class="e-show-more"
+              v-if="$easybooking.match.tickets && filterOptions && $easybooking.match.tickets.length > filterOptions.num_tickets"
+              large
+              color="primary"
+              rounded
+              block
+              v-on:click="filterOptions.num_tickets += 10"
+            >Показать еще</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-layout>
+    <easybooking-footer>{{languages}}</easybooking-footer>
+  </div>
 </template>
 <script>
-import Languagable from '../mixins/language'
+import Languagable from "../mixins/language";
 export default {
-  name: 'Offers',
+  name: "Offers",
   mixins: [Languagable],
   data: () => ({
     actions: [
-      { text: 'About US', to: '/about' },
-      { text: 'Blog', to: '/blog' },
-      { text: 'Our address', to: '/address' }
+      { text: "About US", to: "/about" },
+      { text: "Blog", to: "/blog" },
+      { text: "Our address", to: "/address" }
     ],
-    statics: [
-      '+998933363933', '+998933363933'
-    ],
+    statics: ["+998933363933", "+998933363933"],
     routes: [],
     tickets: [],
     filters: null,
     filterOptions: null
   }),
   methods: {
-    search(){
-      this.routes = this.$refs['search-board'].getFlights()
+    search() {
+      this.routes = this.$refs["search-board"].getFlights();
       this.$etm.search(this.routes, (error, result) => {
-        if(error){
-          this.$etm.alert('Could not search results. Please try again.')
+        if (error) {
+          this.$etm.alert("Could not search results. Please try again.");
+        } else {
+          this.$router.push({ path: "/offers/" + result.request_id });
         }
-        else{
-          this.$router.push({ path: '/offers/' + result.request_id })
-        }
-      })
+      });
     },
-    offers(){
-      this.$etm.offers({ request_id: this.$route.params.id, sort: 'price', last_ow_variant: 0 }, (error, tickets) => {
-        if(error){
-          this.$etm.alert('Offers could not found')
-        }
-        else{
-          if(JSON.stringify(this.$easybooking.match.tickets) !== JSON.stringify(tickets)){
-            this.$easybooking.match = this.$easybooking.Filters(tickets, 'ru')
-            this.filterOptions = this.$easybooking.match.options
-            this.tickets = this.$easybooking.match.search()
+    offers() {
+      this.$etm.offers(
+        {
+          request_id: this.$route.params.id,
+          sort: "price",
+          last_ow_variant: 0
+        },
+        (error, tickets) => {
+          if (error) {
+            this.$etm.alert("Offers could not found");
+          } else {
+            if (
+              JSON.stringify(this.$easybooking.match.tickets) !==
+              JSON.stringify(tickets)
+            ) {
+              this.$easybooking.match = this.$easybooking.Filters(
+                tickets,
+                "ru"
+              );
+              this.filterOptions = this.$easybooking.match.options;
+              this.tickets = this.$easybooking.match.search();
+            }
           }
         }
-      })
+      );
     },
-    reset(){
-      this.$easybooking.match = this.$easybooking.Filters(this.$easybooking.match.tickets, 'ru')
-      this.filterOptions = this.$easybooking.match.options
-      this.tickets = this.$easybooking.match.search()
+    reset() {
+      this.$easybooking.match = this.$easybooking.Filters(
+        this.$easybooking.match.tickets,
+        "ru"
+      );
+      this.filterOptions = this.$easybooking.match.options;
+      this.tickets = this.$easybooking.match.search();
     }
   },
-  mounted(){
-    this.offers()
+  mounted() {
+    this.offers();
   },
-  watch:{
+  watch: {
     filterOptions: {
-      handler(){
-        this.tickets = this.$easybooking.match.search()
+      handler() {
+        this.tickets = this.$easybooking.match.search();
       },
       deep: true
     }
   }
-}
-
+};
 </script>
-<style>
-.offers{
-    background-color: #F4F4F4;
+<style lang="scss">
+.offers {
+  background-color: #f4f4f4;
 }
-.offers-header{
-    background-color: white;
+.offers-header {
+  background-color: white;
 }
-.offers-body{
-    min-height: 700px;
+.offers-body {
+  min-height: 700px;
 }
-  .e-sidebar{
-    padding-right: 30px;
+.e-sidebar {
+  padding-right: 30px;
+}
+.e-show-more{
+  height: 40px;
+  box-shadow: none !important;
+  .v-btn__content{
+    font-weight: 500;
+    font-size: 15px;
+    line-height: 18px;
+    color: #FFFFFF;
+    text-transform: initial;
   }
+}
 </style>
